1fb8f4359b32064ff6219c73a6fe042d1d2c6c5d
AnushaChaitanya <chaitanyaanusha2@gmail.com>
2023-05-29 17:45:44 +0530

    added navbar components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
+
     <div className="App">
       <h1 style={{color:"blue"}}>Cloning YouTube</h1>
-      
+      <Navbar/>
       
     </div>
   );
