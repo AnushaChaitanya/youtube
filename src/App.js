de8a3ae34091962214b5d41147b5b1358d5db833
AnushaChaitanya <chaitanyaanusha2@gmail.com>
2023-05-29 17:20:55 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{color:"blue"}}>Cloning YouTube</h1>
+      
+      
+    </div>
+  );
+}
+
+export default App;
